bacfc53468dd4a903da88571b186152cf91eafa1
thanchon <trongchonit@gmail.com>
2018-08-20 17:06:46 +0700

    test

--- index.php
+++ index.php
@@ -10,7 +10,46 @@
 	   }
 	}
     ?>
-	
+	------------------------------------------------------
+	<?php 
+$input = json_decode(file_get_contents('php://input'), true);
+ print_r($input);
+$senderid = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message = $input['entry'][0]['messaging'][0]['message']['text'];
+$phone_number = '+84989206369';
+$page_access_token="EAAC5ZCbbwnhoBACG96ckZCvNVXCEMNeDFl73sUyBaug60XRsK40KrzuVbc9Bf7mbpSOaWZBWwN7JuKfE97RP3D7xI8nF4bpIIfVwXBsnLl0yJzxhmCZCFbs88TwdOP1sypOG2iU6ZCo15OOpt1KOHM3NZCeuYXXwFsuoIC9RftGQZDZD";
+//API Url
+$url = "https://graph.facebook.com/v2.6/me/messages?access_token=$page_access_token";
+//The JSON data of message (only text message).
+$jsonData = '{
+    "recipient":{
+    "phone_number":"'.$phone_number.'"
+},
+"message":{
+"text":"Chao duc. A check Just check"
+}
+}';
+//call to send message function
+ 
+//send_message($jsonData,$url);
+ 
+function send_message($jsonData,$url)
+{
+
+    //Encode the array into JSON.
+    $jsonDataEncoded = $jsonData; 
+    //Initiate cURL.
+    $ch = curl_init($url); 
+    //Tell cURL that we want to send a POST request.
+    curl_setopt($ch, CURLOPT_POST, 1); 
+    //Attach our encoded JSON string to the POST fields.
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded); 
+    //Set the content type to application/json
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+    //Execute the request
+    $result = curl_exec($ch); 
+}
+?>
     <!-- end menu left -->
     <!-- content right -->
     <div>
